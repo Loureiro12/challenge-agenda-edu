557876453f43d8238d8f03b2e4fa0d25a2178161
André Loureiro <65689062+Loureiro12@users.noreply.github.com>
2022-09-06 21:50:53 -0300

    feat: adding theme

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,11 @@
+export const defaultTheme = {
+  primary: '#f8f9fa',
+  secondary: '#6610f2',
+
+  'green-300': '#00B37E',
+  'green-500': '#00875F',
+  'green-700': '#015F43',
+
+  'red-500': '#AB222E',
+  'red-700': '#7A1921',
+}
