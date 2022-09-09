d92e859682c8be32d9ab0905a85f7b17a0e88e08
André Loureiro <65689062+Loureiro12@users.noreply.github.com>
2022-09-09 01:00:38 -0300

    feat: configuring test

--- src/Router/index.tsx
+++ src/Router/index.tsx
@@ -1,12 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
-import { Home } from './screens/Home'
-import { MovieDetails } from './screens/MovieDetails'
+import { Home } from '../screens/Home'
+import { MovieDetails } from '../screens/MovieDetails'
 
 export function Router() {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
-      <Route path='/movie' element={<MovieDetails />} />
+      <Route path='/movie/:idMovie' element={<MovieDetails />} />
     </Routes>
   )
 }
